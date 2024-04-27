ecf4040f551132d12e4d1b6d86b3ca11425da932
Rex Herndon <herndonrex1@gmail.com>
2024-04-27 13:43:30 -0500

    build search bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,16 @@
-import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/logo.webp'
-import ColorModeSwitch from './ColorModeSwitch'
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent='space-between' padding='10px'>
-        <Image src={logo} boxSize='60px' />
-        <ColorModeSwitch />
+    <HStack padding="10px">
+      <Image src={logo} boxSize="60px" />
+      <SearchInput></SearchInput>
+      <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
